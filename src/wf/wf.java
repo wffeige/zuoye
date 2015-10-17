511dba8f7a648a793c8e792e7c0426d872505caa
wffeige <757626529@qq.com>
2015-10-17 13:11:48 +0800

    zuoye

---
+++ src/wf/wf.java
@@ -0,0 +1,15 @@
+package wf;
+
+public class wf {
+
+	public wf(){
+		
+	}
+	public int add(int a,int b){
+		return a+b;
+	}
+	public int jian(int a,int b){
+		
+		return a-b;
+	}
+}
---
+++ src/wf/wfTest.java
@@ -0,0 +1,32 @@
+package wf;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class wfTest {
+
+	wf wwf = new wf();
+	@Before
+	public void setUp() throws Exception {
+		wwf = new wf();
+	}
+
+	@Test
+	public void testAdd() {
+		//fail("尚未实现");
+	   int x;
+	   x=wwf.add(2, 3);
+	   assertEquals(5, x);
+	}
+
+	@Test
+	public void testJian() {
+		//fail("尚未实现");
+		int x;
+		x=wwf.jian(4, 2);
+		assertEquals(2, x);
+	}
+
+}
